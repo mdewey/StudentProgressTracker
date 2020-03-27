d50b60a0b3c9764733435a6e3975ab45019e54d3
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-27 13:39:56 -0400

    styled cohort page

---
+++ ClientApp/src/components/CohortCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 200,
+  },
+  media: {
+    height: 200,
+  },
+})
+
+const CohortCard = ({ cohort }) => {
+  const classes = useStyles()
+  const bull = <span className={classes.bullet}>•</span>
+
+  return (
+    <>
+      <Link to={`/cohort/${cohort.id}`} className="cohort-card">
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={`https://api.adorable.io/avatars/200/${cohort.name}`}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                <div>{cohort.name}</div>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Link>
+    </>
+  )
+}
+
+export default CohortCard
--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -2,9 +2,20 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import { createBrotliCompress } from 'zlib'
-import CohortLink from '../components/CohortLink'
+import CohortCard from '../components/CohortCard'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+}))
 
 const Home = () => {
+  const classes = useStyles()
   const [currentCohorts, setCurrentCohorts] = useState([])
   const [allCohorts, setAllCohorts] = useState([])
 
@@ -23,21 +34,27 @@ const Home = () => {
 
   return (
     <div>
+      <br />
       <section>
-        <header>Current Cohorts</header>
-        <ul>
-          {currentCohorts.map(cohort => {
-            return <CohortLink key={cohort.id} cohort={cohort} />
-          })}
-        </ul>
+        <h2>Current Cohorts</h2>
+        <hr />
+        {currentCohorts.map(cohort => {
+          return <CohortCard key={cohort.id} cohort={cohort} />
+        })}
+        <br />
       </section>
-      <section>
-        <header>All Cohorts</header>
-        <ul>
+      <section className={classes.root}>
+        <h3>All Cohorts</h3>
+        <hr />
+        <Grid container spacing={3}>
           {allCohorts.map(cohort => {
-            return <CohortLink key={cohort.id} cohort={cohort} />
+            return (
+              <Grid item xs>
+                <CohortCard key={cohort.id} cohort={cohort} />
+              </Grid>
+            )
           })}
-        </ul>
+        </Grid>
       </section>
     </div>
   )
